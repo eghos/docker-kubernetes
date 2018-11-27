55713f741dba7eb0ae66726d427af2ce4318fe0d
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-27 09:35:28 +0100

    jenkins shared lib config

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,15 +2,15 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was successful"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} was successful!\n Build URL at: ${env.BUILD_URL}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was failed"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} has failed!\n Build URL at: ${env.BUILD_URL}"
   }
   else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} was unstable"
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} was unstable!\n Build URL at: ${env.BUILD_URL}"
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} its resulat was unclear"	
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} - its result was unclear. Please investigate!\n Build URL at: ${env.BUILD_URL}"
   }
 }
