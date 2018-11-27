e91cde9ec9a65d62b921a5f4735b0a6b925aa27d
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-27 21:34:30 +0100

    pipeline groovy updates

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -1,8 +1,10 @@
 #!/usr/bin/env groovy
 
 def call(String buildResult) {
+  JOB_URL_HTTPS = env.BUILD_URL.replace('http','https')
+
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} was Successful!"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} was Successful!\n Build URL: ${JOB_URL_HTTPS}"
   }
   else if( buildResult == "FAILURE" ) { 
     slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} has Failed!"
