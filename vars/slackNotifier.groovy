41686243ed9b6e5f7eacb42dd1d9c7feffdd1d45
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-27 11:50:06 +0100

    pipeline updates

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -2,15 +2,17 @@
 
 def call(String buildResult) {
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} was successful!"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} was Successful!"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} has failed!"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} has Failed!"
   }
-  else if( buildResult == "UNSTABLE" ) { 
-    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} was unstable!"
+  else if( buildResult == "UNSTABLE" ) {
+    slackSend color: "warning", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} was Unstable!"
+  }
+  else if( buildResult == "ABORTED" ) {
   }
   else {
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with buildnumber ${env.BUILD_NUMBER} by ${env.USER_ID} - its result was unclear. Please investigate!\n Build URL at: ${env.BUILD_URL}"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} by ${env.USER_ID} - its result was unclear. Please investigate!\n Build URL at: ${env.BUILD_URL}"
   }
 }
