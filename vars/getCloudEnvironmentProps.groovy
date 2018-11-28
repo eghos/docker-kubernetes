c1350fab4179cabf7294415fe25534b0e5881789
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-28 21:04:34 +0100

    properties update

--- vars/getCloudEnvironmentProps.groovy
+++ vars/getCloudEnvironmentProps.groovy
@@ -2,7 +2,7 @@
 
 def call(String property) {
     Properties props = new Properties()
-    File propsFile = new File('CloudPlatform.properties')
+    File propsFile = new File('/vars/CloudPlatform.properties')
     props.load(propsFile.newDataInputStream())
 
     return props.getProperty(property)
