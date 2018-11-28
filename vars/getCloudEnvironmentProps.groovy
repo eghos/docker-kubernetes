85d229b60de81196e9e9a150edfd0f3a357d92cd
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-28 16:24:52 +0100

    pipeline groovy updates

---
+++ vars/getCloudEnvironmentProps.groovy
@@ -0,0 +1,9 @@
+#!/usr/bin/env groovy
+
+def call(String property) {
+    Properties props = new Properties()
+    File propsFile = new File('CloudPlatform.properties')
+    props.load(propsFile.newDataInputStream())
+
+    return props.getProperty(property)
+}
