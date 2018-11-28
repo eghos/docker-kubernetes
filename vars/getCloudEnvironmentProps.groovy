dd47baa440cc91685262f8b732e198577723908d
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-28 21:23:00 +0100

    properties update

--- vars/getCloudEnvironmentProps.groovy
+++ vars/getCloudEnvironmentProps.groovy
@@ -1,13 +1,14 @@
 #!/usr/bin/env groovy
 
-
-
 def call(String property) {
 //    Properties props = new Properties()
 //    File propsFile = new File('/CloudPlatform.properties')
 //    props.load(propsFile.newDataInputStream())
 //
 //    return props.getProperty(property)
-    String test = "hello"
-    return test
+    String AZURE_DEV_WESTEUROPE_DNS = "dev-az-svc.westeurope.cloudapp.azure.com"
+    String GIT_SVC_ACOUNT_EMAIL = "l-apimgt-u-itsehbg@ikea.com"
+    String GIT_SVC_ACCOUNT_USER = "l-apimgt-u-itsehbg"
+
+    return property
 }
