7c436c8148b2997200d1b081b401da71915bac7c
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-29 10:48:42 +0100

    properties update

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -434,16 +434,18 @@ def call(Map pipelineParams) {
                 steps {
                     withCredentials([sshUserPrivateKey(credentialsId: 'l-apimgt-u-itsehbgATikea.com', keyFileVariable: 'SSH_KEY')]) {
                         withEnv(["GIT_SSH_COMMAND=ssh -o StrictHostKeyChecking=no -o User=${GIT_USER} -i ${SSH_KEY}"]) {
-                            
                             script {
-                                 sh 'git remote rm origin'
-                                 sh 'git remote add origin "git@git.build.ingka.ikea.com:IPIM-IP/price-service.git"'
+                                sh 'git remote rm origin'
+                                sh 'git remote add origin "git@git.build.ingka.ikea.com:IPIM-IP/price-service.git"'
 //                                 sh "git remote set-url origin ${GIT_URL}"
 
+                                // Below line could be causing the intermittent error for Git SCM, if true then need to remove and find alternate
+//                                sh 'git config url."git@git.build.ingka.ikea.com:".insteadOf "https://git.build.ingka.ikea.com/"'
                                 sh 'git config --global user.email "l-apimgt-u-itsehbg@ikea.com"'
                                 sh 'git config --global user.name "l-apimgt-u-itsehbg"'
                                 sh 'git add pom.xml'
                                 sh 'git commit -am "System - Update POM Version [ci skip]"'
+//                                sh "git push origin HEAD"
                                 sh 'git push origin "${BRANCH_NAME_FULL}"'
                             }
                         }
