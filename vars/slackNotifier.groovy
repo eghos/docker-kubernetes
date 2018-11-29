562eeac37ae2a516fb8a7a08bc758f02d06bed04
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-29 17:14:19 +0100

    properties update

--- vars/slackNotifier.groovy
+++ vars/slackNotifier.groovy
@@ -4,10 +4,10 @@ def call(String buildResult) {
   JOB_URL_HTTPS = env.BUILD_URL.replace('http','https')
 
   if ( buildResult == "SUCCESS" ) {
-    slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} was Successful! :)\n Build URL: ${JOB_URL_HTTPS}"
+    slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} was Successful!\n Build URL: ${JOB_URL_HTTPS}"
   }
   else if( buildResult == "FAILURE" ) { 
-    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} has Failed! :(\n Build URL: ${JOB_URL_HTTPS}"
+    slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} has Failed!\n Build URL: ${JOB_URL_HTTPS}"
   }
   else if( buildResult == "UNSTABLE" ) {
     slackSend color: "warning", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} was Unstable!\n Build URL: ${JOB_URL_HTTPS}"
