4afac882aa08e81d35986a6ceb1cc130393b57ef
Onkar Shoker <onkar.shoker@ikea.com>
2018-11-29 11:46:58 +0100

    properties update

--- vars/getCloudEnvironmentProps.groovy
+++ vars/getCloudEnvironmentProps.groovy
@@ -1,19 +1,26 @@
-#!/usr/bin/env groovy
-def call(String property) {
-    def AZURE_DEV_WESTEUROPE_DNS            = "dev-az-svc.westeurope.cloudapp.azure.com"
-    def AZURE_SVC_HOSTNAME                  = "<ENV>-az-svc.<REGION>.cloudapp.azure.com"
-    def GIT_SVC_ACOUNT_EMAIL                = "l-apimgt-u-itsehbg@ikea.com"
-    def GIT_SVC_ACCOUNT_USER                = "l-apimgt-u-itsehbg"
-    def NONPROD_WESTEUROPE_AZRGNAME         = "ipimip-dev-westEurope-rg"
-    def NONPROD_WESTEUROPE_AZACRNAME        = "acrwedevgupuy7"
-    def NONPROD_WESTEUROPE_AZAKSCLUSTERNAME = "akswedevgupuy7"
-    def PROD_WESTEUROPE_AZRGNAME            = "ipimip-ppe-westEurope-rg"
-    def PROD_WESTEUROPE_AZACRNAME           = "acrweppeafsibk"
-    def PROD_WESTEUROPE_AZAKSCLUSTERNAME    = "aksweppeafsibk"
-    return property
-}
-def test_var = "qwerty"
+//#!/usr/bin/env groovy
+//def call(String property) {
+//    def AZURE_DEV_WESTEUROPE_DNS            = "dev-az-svc.westeurope.cloudapp.azure.com"
+//    def AZURE_SVC_HOSTNAME                  = "<ENV>-az-svc.<REGION>.cloudapp.azure.com"
+//    def GIT_SVC_ACOUNT_EMAIL                = "l-apimgt-u-itsehbg@ikea.com"
+//    def GIT_SVC_ACCOUNT_USER                = "l-apimgt-u-itsehbg"
+//    def NONPROD_WESTEUROPE_AZRGNAME         = "ipimip-dev-westEurope-rg"
+//    def NONPROD_WESTEUROPE_AZACRNAME        = "acrwedevgupuy7"
+//    def NONPROD_WESTEUROPE_AZAKSCLUSTERNAME = "akswedevgupuy7"
+//    def PROD_WESTEUROPE_AZRGNAME            = "ipimip-ppe-westEurope-rg"
+//    def PROD_WESTEUROPE_AZACRNAME           = "acrweppeafsibk"
+//    def PROD_WESTEUROPE_AZAKSCLUSTERNAME    = "aksweppeafsibk"
+//    return property
+//}
+//def test_var = "qwerty"
+//
+//def getTest(){
+//    return "${test_var}"
+//}
+class getCloudEnvironmentProps{
+    String test_var = "qwert"
 
-def getTest(){
-    return "${test_var}"
+    def getTest(){
+        return test_var
+    }
 }
