d738ef923ee87d93a3a55ed31c8ab03f4245cae6
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-02 13:16:59 +0100

    config updates

--- vars/cloudEnvironmentProps.groovy
+++ vars/cloudEnvironmentProps.groovy
@@ -1,27 +1,24 @@
 //#!/usr/bin/env groovy
 class cloudEnvironmentProps{
     def AZURE_DEV_WESTEUROPE_DNS            = "dev-az-svc.westeurope.cloudapp.azure.com"
-    def AZURE_SVC_HOSTNAME                  = "<ENV>-az-svc.<REGION>.cloudapp.azure.com"
-    def GIT_SVC_ACOUNT_EMAIL                = "l-apimgt-u-itsehbg@ikea.com"
-    def GIT_SVC_ACCOUNT_USER                = "l-apimgt-u-itsehbg"
+    def AZURE_TEST_WESTEUROPE_DNS           = "test-az-svc.westeurope.cloudapp.azure.com"
+    def AZURE_PPE_WESTEUROPE_DNS            = "ppe-az-svc.westeurope.cloudapp.azure.com"
+    def AZURE_PROD_WESTEUROPE_DNS           = "prod-az-svc.westeurope.cloudapp.azure.com"
+
     def NONPROD_WESTEUROPE_AZRGNAME         = "ipimip-dev-westEurope-rg"
     def NONPROD_WESTEUROPE_AZACRNAME        = "acrwedevgupuy7"
     def NONPROD_WESTEUROPE_AZAKSCLUSTERNAME = "akswedevgupuy7"
+
     def PROD_WESTEUROPE_AZRGNAME            = "ipimip-ppe-westEurope-rg"
     def PROD_WESTEUROPE_AZACRNAME           = "acrweppeafsibk"
     def PROD_WESTEUROPE_AZAKSCLUSTERNAME    = "aksweppeafsibk"
 
+    def AZURE_SVC_HOSTNAME                  = "<ENV>-az-svc.<REGION>.cloudapp.azure.com"
+    def GIT_SVC_ACOUNT_EMAIL                = "l-apimgt-u-itsehbg@ikea.com"
+    def GIT_SVC_ACCOUNT_USER                = "l-apimgt-u-itsehbg"
+
     def getAzureDevWesteuropeDns(){
         return AZURE_DEV_WESTEUROPE_DNS
-    }
-    def getAzureSvcHostname(){
-        return AZURE_SVC_HOSTNAME
-    }
-    def getGitSvcAccountEmail(){
-        return GIT_SVC_ACOUNT_EMAIL
-    }
-    def getGitSvcAccountUser(){
-        return GIT_SVC_ACCOUNT_USER
     }
     def getNonProdWesteuropeAzRgName(){
         return NONPROD_WESTEUROPE_AZRGNAME
@@ -40,5 +37,15 @@ class cloudEnvironmentProps{
     }
     def getProdWesteuropeAzAksClusterName(){
         return PROD_WESTEUROPE_AZAKSCLUSTERNAME
+    }
+
+    def getAzureSvcHostname(){
+        return AZURE_SVC_HOSTNAME
+    }
+    def getGitSvcAccountEmail(){
+        return GIT_SVC_ACOUNT_EMAIL
+    }
+    def getGitSvcAccountUser(){
+        return GIT_SVC_ACCOUNT_USER
     }
 }
