5613f34072195a39f7b66b6331d1535e124b0e01
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-03 16:49:20 +0100

    config updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -315,8 +315,9 @@ def call(Map pipelineParams) {
             stage('Fetch Apiary Definition') {
                 steps {
                     script {
-                        TEST1 = ${APIARY_PROJECT_NAME}
-                        echo "${TEST1}"
+                        echo "hey!"
+//                        TEST1 = ${APIARY_PROJECT_NAME}
+//                        echo "${TEST1}"
 //                        sh """export APIARY_PROJECT_NAME_VAR=${APIARY_PROJECT_NAME}
 //                          cd ./build
 //                          export APIARY_API_KEY=890e555a1d3107539c130f23b9494155
