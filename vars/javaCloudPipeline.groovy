c2d9a702d139f6d458eec9012a994883d233c5a1
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-03 14:43:02 +0100

    config updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -119,7 +119,37 @@ def call(Map pipelineParams) {
             stage('Update Versions') {
                 steps {
                     withCredentials([sshUserPrivateKey(credentialsId: 'l-apimgt-u-itsehbgATikea.com', keyFileVariable: 'SSH_KEY')]) {
-                        stageUpdateVersions()
+                        sh 'chmod +x ./mvnw'
+                        script {
+                            if (env.BRANCH_NAME.startsWith("PR")) {
+                                echo 'This is a PR Branch'
+//                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${DEV_PR_VERSION}'
+                            }
+
+                            if (env.BRANCH_NAME.startsWith("develop")) {
+                                echo 'This is a develop Branch'
+                                //Update pom.xml version
+                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=1.0.${BUILD_NUMBER}-SNAPSHOT'
+                                DOCKER_VERSION = "${DEV_SNAPSHOT_VERSION}"
+                            }
+
+                            if (env.BRANCH_NAME.startsWith("release/")) {
+                                echo 'This is a release Branch'
+                                //Update pom.xml version
+                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${RELEASE_VERSION}'
+                                DOCKER_VERSION = "${RELEASE_NUMBER}"
+
+                            }
+
+                            if (env.BRANCH_NAME.startsWith("master")) {
+                                echo 'This is a master Branch'
+                            }
+
+                            if (env.BRANCH_NAME.startsWith("hotfix")) {
+                                echo 'This is a hotfix Branch - TODO Inc Hotfix PATCH'
+                            }
+                        }
+                        echo readMavenPom().getVersion()
                     }
                 }
             }
@@ -280,6 +310,14 @@ def call(Map pipelineParams) {
                         parallel AZURE_TEST_REGION_MAP
                     }
                 }
+            }
+
+            stage('Fetch Apiary Definition') {
+                steps {
+                    sh 'export APIARY_API_KEY=890e555a1d3107539c130f23b9494155'
+                    sh 'apiary fetch --api-name saferhomeapi'
+                }
+
             }
 
             stage('Service Tests') {
