15b6d2ada50898b777c9b183c8db63ad672e2bc0
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-03 11:56:03 +0100

    config updates

--- vars/stageSkipCICD.groovy
+++ vars/stageSkipCICD.groovy
@@ -1,11 +1,7 @@
 def call() {
-    stage("Skip CICD?") {
-        steps {
-            script {
-                echo 'Got ci=skip, aborting build'
-                currentBuild.result = 'ABORTED'
-                error('CI-Skip')
-            }
-        }
+    script {
+        echo 'Got ci=skip, aborting build'
+        currentBuild.result = 'ABORTED'
+        error('CI-Skip')
     }
 }
