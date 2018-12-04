fda34b514c8697c630b338f6a9f7464ccd38fe5a
James Neate <james.neate@ikea.com>
2018-12-04 12:41:42 +0000

    Re-added the setup general stage

--- vars/nodeCloudPipeline.groovy
+++ vars/nodeCloudPipeline.groovy
@@ -69,6 +69,35 @@ def call(Map pipelineParams) {
 
         stages {
 
+            stage('Setup General') {
+               steps {
+                   stageSetupGeneral()
+                   script {
+                       deploymentProperties = readProperties file:'deployment.properties'
+
+                       DEPLOY_TO_AWS       = deploymentProperties['DEPLOY_TO_AWS']
+
+                       AWS_DEV_REGION      = deploymentProperties['AWS_DEV_REGION'].split(',').collect{it as String}
+                       AWS_TEST_REGION     = deploymentProperties['AWS_TEST_REGION'].split(',').collect{it as String}
+                       AWS_PROD_REGION     = deploymentProperties['AWS_PROD_REGION'].split(',').collect{it as String}
+
+                       DEPLOY_TO_AZURE     = deploymentProperties['DEPLOY_TO_AZURE']
+
+                       AZURE_DEV_REGION    = deploymentProperties['AZURE_DEV_REGION'].split(',').collect{it as String}
+                       AZURE_TEST_REGION   = deploymentProperties['AZURE_TEST_REGION'].split(',').collect{it as String}
+                       AZURE_PROD_REGION   = deploymentProperties['AZURE_PROD_REGION'].split(',').collect{it as String}
+
+                       DEPLOY_TO_ON_PREM   = deploymentProperties['DEPLOY_TO_ON_PREM']
+                       ON_PREM_REGION      = deploymentProperties['ON_PREM_REGION']
+
+                       APIARY_PROJECT_NAME = deploymentProperties['APIARY_PROJECT_NAME']
+
+                       URI_ROOT_PATH       = deploymentProperties['URI_ROOT_PATH']
+
+                   }
+               }
+           }
+
             stage("Skip CICD?") {
                 when {
                     expression {
