30710e07ed813239ff3eda0dba9bb21e56c668d5
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-07 21:37:01 +0100

    updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -482,6 +482,8 @@ def generateAzureDeployStage(region, env) {
         stage("${region}") {
             withCredentials([azureServicePrincipal('sp-ipim-ip-aks')]) {
                 script {
+                    sh "echo ${region}"
+                    sh "echo ${env}"
                     ACRLOGINSERVER = sh(returnStdout: true, script: "az acr show --resource-group ${AZ_RG_NAME} --name ${AZ_ACR_NAME} --query \"loginServer\" --output tsv").trim()
                     AZ_ENV_REGION_SVC_HOSTNAME = "${AZURE_DEV_WESTEUROPE_DNS_PROP}".replace('<ENV>', "${env}").replace('<REGION>', "${region}")
                     sh "echo ${ACRLOGINSERVER}"
