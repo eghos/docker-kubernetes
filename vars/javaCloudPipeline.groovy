52550eb0c366535f91cebeb0f1f2a312427914fd
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-14 14:26:27 +0100

    updates for dredd

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -302,8 +302,8 @@ def call(Map pipelineParams) {
 
                         sh """
                            cd build
-                           sed -i -e \"s|APIARY_PROJECT_VAR|${APIARY_PROJECT_NAME}.apib|g\" dredd.yaml
-                           sed -i -e \"s|SERVICE_GATEWAY_DNS_VAR|${SERVICE_GATEWAY_DNS_PROP}|g\" dredd.yaml"""
+                           sed -i -e \"s|APIARY_PROJECT_VAR|${APIARY_PROJECT_NAME}.apib|g\" dredd.yml
+                           sed -i -e \"s|SERVICE_GATEWAY_DNS_VAR|${SERVICE_GATEWAY_DNS_PROP}|g\" dredd.yml"""
 
                         sh 'docker run -i -v ${WORKSPACE}/build:/api -w /api apimgt/dredd'
                         sh 'cd ${WORKSPACE}/build && ls -lart'
