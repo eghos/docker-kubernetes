eaaa2025d0bbeb93f13f096d8a6b30cc818e7740
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-19 20:03:24 +0100

    updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -190,7 +190,7 @@ def call(Map pipelineParams) {
                     sh """
                        cd build/api-blueprint
                        sed -i -e \"s|APIARY_PROJECT_VAR|${APIARY_PROJECT_NAME}.apib|g\" dredd.yml
-                       sed -i -e \"s|SERVICE_GATEWAY_DNS_VAR|${SERVICE_GATEWAY_DNS_PROP}${URI_ROOT_PATH}|g\" dredd.yml
+                       sed -i -e \"s|SERVICE_GATEWAY_DNS_VAR|${SERVICE_GATEWAY_DNS_PROP}|g\" dredd.yml
                        """
 
                     //Run Dredd Test against APIB Definition and running service.
