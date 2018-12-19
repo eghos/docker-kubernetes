e71a5bb86c088daff54e62bbda42f938d51ad931
Onkar Shoker <onkar.shoker@ikea.com>
2018-12-19 11:29:35 +0100

    updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -138,7 +138,11 @@ def call(Map pipelineParams) {
                             }
 
                             //Log into ACR/ECR etc
-                            logIntoAzure()
+                            sh "az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} -t ${AZURE_TENANT_ID}"
+                            sh "az account set -s ${AZURE_SUBSCRIPTION_ID}"
+                            sh "az acr login --name ${PROD_WESTEUROPE_AZACRNAME_PROP}"
+                            ACRLOGINSERVER = sh(returnStdout: true, script: "az acr show --resource-group ${PROD_WESTEUROPE_AZRGNAME_PROP} --name ${PROD_WESTEUROPE_AZACRNAME_PROP} --query \"loginServer\" --output tsv").trim()
+
                         }
                     }
                 }
@@ -146,37 +150,41 @@ def call(Map pipelineParams) {
 
             stage('Update Versions') {
                 steps {
-                        sh 'chmod +x ./mvnw'
-                        script {
-                            if (env.BRANCH_NAME.startsWith("PR")) {
-                                echo 'This is a PR Branch'
-                            }
+//                    withCredentials([sshUserPrivateKey(credentialsId: 'l-apimgt-u-itsehbgATikea.com', keyFileVariable: 'SSH_KEY')]) {
+                    sh 'chmod +x ./mvnw'
+                    script {
+//                            if (env.BRANCH_NAME.startsWith("PR")) {
+//                                echo 'This is a PR Branch'
+//                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${DEV_PR_VERSION}'
+//                            }
 
-                            if (env.BRANCH_NAME.startsWith("develop")) {
-                                echo 'This is a develop Branch'
-                                //Update pom.xml version
-                                // sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=1.0.${BUILD_NUMBER}-SNAPSHOT'
-                                DOCKER_VERSION = "${DEV_SNAPSHOT_VERSION}"
-                            }
-
-                            if (env.BRANCH_NAME.startsWith("release/")) {
-                                echo 'This is a release Branch'
-                                //Update pom.xml version
-                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${RELEASE_VERSION}'
-                                DOCKER_VERSION = "${RELEASE_NUMBER}"
-                            }
-
-                            if (env.BRANCH_NAME.startsWith("master")) {
-                                echo 'This is a master Branch'
-                                DOCKER_VERSION = "${PROD_RELEASE_NUMBER}"
-                            }
-
-                            if (env.BRANCH_NAME.startsWith("hotfix")) {
-                                echo 'This is a hotfix Branch - TODO Inc Hotfix PATCH'
-                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${RELEASE_VERSION}'
-                                DOCKER_VERSION = "${RELEASE_NUMBER}"
-                            }
+                        if (env.BRANCH_NAME.startsWith("develop")) {
+                            echo 'This is a develop Branch'
+                            //Update pom.xml version
+//                                sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=1.0.${BUILD_NUMBER}-SNAPSHOT'
+                            DOCKER_VERSION = "${DEV_SNAPSHOT_VERSION}"
                         }
+
+                        if (env.BRANCH_NAME.startsWith("release/")) {
+                            echo 'This is a release Branch'
+                            //Update pom.xml version
+                            sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${RELEASE_VERSION}'
+                            DOCKER_VERSION = "${RELEASE_NUMBER}"
+                        }
+
+                        if (env.BRANCH_NAME.startsWith("master")) {
+                            echo 'This is a master Branch'
+                            DOCKER_VERSION = "${PROD_RELEASE_NUMBER}"
+                        }
+
+                        if (env.BRANCH_NAME.startsWith("hotfix")) {
+                            echo 'This is a hotfix Branch - TODO Inc Hotfix PATCH'
+                            sh './mvnw -B org.codehaus.mojo:versions-maven-plugin:2.5:set -DprocessAllModules -DnewVersion=${RELEASE_VERSION}'
+                            DOCKER_VERSION = "${RELEASE_NUMBER}"
+                        }
+                    }
+                    echo readMavenPom().getVersion()
+//                    }
                 }
             }
 
@@ -249,6 +257,36 @@ def call(Map pipelineParams) {
                 steps {
                     withCredentials([azureServicePrincipal('sp-ipim-ip-aks')]) {
                         script {
+//                                AWS_DEV_REGION_MAP = AWS_DEV_REGION.collectEntries {
+//                                    ["${it}" : generateAwsDeployStage(it, "dev")]
+//                                }
+//                                AWS_TEST_REGION_MAP = AWS_TEST_REGION.collectEntries {
+//                                    ["${it}" : generateAwsDeployStage(it, "test")]
+//                                }
+//                                AWS_PPE_REGION_MAP = AWS_PPE_REGION.collectEntries {
+//                                    ["${it}" : generateAwsDeployStage(it, "ppe")]
+//                                }
+//                                AWS_PROD_REGION_MAP = AWS_PROD_REGION.collectEntries {
+//                                    ["${it}" : generateAwsDeployStage(it, "prod")]
+//                                }
+//
+//                                AZURE_DEV_REGION_MAP = AZURE_DEV_REGION.collectEntries {
+//                                    ["${it}" : generateAzureDeployStage(it, "dev")]
+//                                }
+//                                AZURE_TEST_REGION_MAP = AZURE_TEST_REGION.collectEntries {
+//                                    ["${it}" : generateAzureDeployStage(it, "test")]
+//                                }
+//                                AZURE_PPE_REGION_MAP = AZURE_PPE_REGION.collectEntries {
+//                                    ["${it}" : generateAzureDeployStage(it, "ppe")]
+//                                }
+//                                AZURE_PROD_REGION_MAP = AZURE_PROD_REGION.collectEntries {
+//                                    ["${it}" : generateAzureDeployStage(it, "prod")]
+//                                }
+
+//                            sh "az login --service-principal -u ${AZURE_CLIENT_ID} -p ${AZURE_CLIENT_SECRET} -t ${AZURE_TENANT_ID}"
+//                            sh "az account set -s ${AZURE_SUBSCRIPTION_ID}"
+//                            sh "az acr login --name ${PROD_WESTEUROPE_AZACRNAME_PROP}"
+//                            ACRLOGINSERVER = sh(returnStdout: true, script: "az acr show --resource-group ${PROD_WESTEUROPE_AZRGNAME_PROP} --name ${PROD_WESTEUROPE_AZACRNAME_PROP} --query \"loginServer\" --output tsv").trim()
                             sh "docker build -t ${ACRLOGINSERVER}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION} ."
                             sh "docker push ${ACRLOGINSERVER}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION}"
 
@@ -305,6 +343,26 @@ def call(Map pipelineParams) {
                 }
             }
 
+            //TODO move contents to Dredd Test in service test
+            stage('Fetch Apiary Definition') {
+                when {
+                    allOf {
+                        branch "release/*";
+                        expression { IS_API_APPLICATION == 'true' }
+                    }
+                }
+                steps {
+                    script {
+                        sh """
+                          cd ./build
+                          export APIARY_API_KEY=${APIARY_IO_TOKEN_PROP}
+                          apiary fetch --api-name ${APIARY_PROJECT_NAME} --output ${APIARY_PROJECT_NAME}.apib
+                          """
+                        sh "git add ./build/${APIARY_PROJECT_NAME}.apib"
+                    }
+                }
+            }
+
             stage('Service Tests') {
                 when {
                     allOf {
@@ -320,24 +378,14 @@ def call(Map pipelineParams) {
                     }
                     stage('Dredd Tests (API Contract)') {
                         steps {
-                            //Fetch API Definition from apiary.io using apiary CLI.
-                            script {
-                                sh """
-                                   cd ./build
-                                   export APIARY_API_KEY=${APIARY_IO_TOKEN_PROP}
-                                   apiary fetch --api-name ${APIARY_PROJECT_NAME} --output ${APIARY_PROJECT_NAME}.apib
-                                   """
-                                sh "git add ./build/${APIARY_PROJECT_NAME}.apib"
-                            }
-                            //Make copy of dredd-template (to stop git automatically checking in existing modified file
                             sh 'cp ./build/dredd-template.yml ./build/dredd.yml'
-                            //Replace variables in Dredd file
+
                             sh """
                                cd build
                                sed -i -e \"s|APIARY_PROJECT_VAR|${APIARY_PROJECT_NAME}.apib|g\" dredd.yml
                                sed -i -e \"s|SERVICE_GATEWAY_DNS_VAR|${SERVICE_GATEWAY_DNS_PROP}${URI_ROOT_PATH}|g\" dredd.yml
                                """
-                            //Run Dredd test.
+
                             script {
                                 try {
                                     sh 'docker run -i -v ${WORKSPACE}/build:/api -w /api apiaryio/dredd'
@@ -345,7 +393,7 @@ def call(Map pipelineParams) {
                                 } catch (err) {
                                     //sh 'chmod +x ./build/results.xml'
                                     sh 'cd ./build && ls -lart'
-                                    //sh "git add ./build/results.xml"
+//                                    sh "git add ./build/results.xml"
                                     echo 'Get XUnit/JUnit Results if available'
                                     //junit './build/results.xml'
                                 }
@@ -508,12 +556,12 @@ def generateAzureDeployStage(region, env) {
                     sh 'chmod +x ./build/*.yaml'
                     sh """
                         cd build
-                        cp \"configmap-az-${region}-${env}.yaml\" \"configmap-az-${region}-${env}-azure.yaml\"
+                        cp \"configmap-${region}-${env}.yaml\" \"configmap-${region}-${env}-azure.yaml\"
                         cp \"deploy-service.yaml\" \"deploy-service-azure.yaml\"
                         cp \"virtual-service.yaml\" \"virtual-service-azure.yaml\"
                         
-                        sed -i -e \"s|KUBERNETES_NAMESPACE_VAR|${KUBERNETES_NAMESPACE}|g\" configmap-az-${region}-${env}-azure.yaml
-                        kubectl apply -f configmap-az-${region}-${env}-azure.yaml
+                        sed -i -e \"s|KUBERNETES_NAMESPACE_VAR|${KUBERNETES_NAMESPACE}|g\" configmap-${region}-${env}-azure.yaml
+                        kubectl apply -f configmap-${region}-${env}-azure.yaml
 
                         sed -i -e \"s|IMAGE_NAME_VAR|${ACRLOGINSERVER}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION}|g\" deploy-service-azure.yaml
                         sed -i -e \"s|SERVICE_NAME_VAR|${IMAGE_NAME}|g\" deploy-service-azure.yaml
