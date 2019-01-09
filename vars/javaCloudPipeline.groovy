7ae49a89302ded8db6d5614a61cf5decffc04e79
Onkar Shoker <onkar.shoker@ikea.com>
2019-01-09 16:34:19 +0100

    update to AZURE_SVC_HOSTNAME global property to reflect naming standard changes

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -592,7 +592,7 @@ def generateAzureDeployStage(region, env) {
                         sed -i -e \"s|CONFIGMAP_NAME_VAR|${IMAGE_NAME}-configmap|g\" deploy-service-azure.yaml
                         sed -i -e \"s|VERSION_VAR|${DOCKER_VERSION}|g\" deploy-service-azure.yaml
                         kubectl apply -f deploy-service-azure.yaml
-                        
+                            
                         sed -i -e \"s|KUBERNETES_NAMESPACE_VAR|${KUBERNETES_NAMESPACE}|g\" virtual-service-azure.yaml
                         sed -i -e \"s|INTERNAL_SVC_HOSTNAME_VAR|${AZ_ENV_REGION_SVC_HOSTNAME}|g\" virtual-service-azure.yaml
                         sed -i -e \"s|SERVICE_NAME_VAR|${IMAGE_NAME}|g\" virtual-service-azure.yaml
