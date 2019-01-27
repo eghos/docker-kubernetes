fe53af7ba35427aec07fd7f471b6cae8c9e0230e
Onkar Shoker <onkar.shoker@ikea.com>
2019-01-27 20:57:42 +0100

    updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -255,7 +255,8 @@ def call(Map pipelineParams) {
                 when {
                     allOf {
                         branch "develop*";
-                        expression { DEPLOY_TO_AWS eq 'true' }
+//                        expression { DEPLOY_TO_AWS == 'true' }
+                        expression { return DEPLOY_TO_AWS != null }
                     }
                 }
                 steps {
