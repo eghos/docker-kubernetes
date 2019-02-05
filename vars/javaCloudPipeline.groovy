549e34091f855f5e8b46a3f38659b47d942cbacc
Onkar Shoker <onkar.shoker@ikea.com>
2019-02-05 10:56:23 +0100

    updates

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -537,7 +537,7 @@ def call(Map pipelineParams) {
         post {
             always {
                 cleanWs()
-//                slackNotifier(currentBuild.currentResult)
+                slackNotifier(currentBuild.currentResult)
             }
         }
 
@@ -713,5 +713,24 @@ def runDreddTest(){
             //TODO
             echo 'Dredd Test failed. Continuing with pipeline'
         }
+    }
+}
+
+def call(String buildResult) {
+    JOB_URL_HTTPS = env.BUILD_URL.replace('http','https')
+
+    if ( buildResult == "SUCCESS" ) {
+        slackSend color: "good", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} was Successful!\n Build URL: ${JOB_URL_HTTPS}"
+    }
+    else if( buildResult == "FAILURE" ) {
+        slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} has Failed!\n Build URL: ${JOB_URL_HTTPS}"
+    }
+    else if( buildResult == "UNSTABLE" ) {
+        slackSend color: "warning", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} was Unstable!\n Build URL: ${JOB_URL_HTTPS}"
+    }
+    else if( buildResult == "ABORTED" ) {
+    }
+    else {
+        slackSend color: "danger", message: "Job: ${env.JOB_NAME} with Build Number ${env.BUILD_NUMBER} - its result was unclear. Please investigate!\n Build URL: ${JOB_URL_HTTPS}"
     }
 }
