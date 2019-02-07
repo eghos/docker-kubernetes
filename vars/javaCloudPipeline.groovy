e2485a267fd745dd3514436bdee2e9e3be7fc630
Onkar Shoker <onkar.shoker@ikea.com>
2019-02-07 09:55:27 +0100

    deploy

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -72,7 +72,36 @@ def call(Map pipelineParams) {
                     }
                 }
                 steps {
-                    stageSkipCICD()
+//                    stageSkipCICD()
+
+                    sh 'which java'
+                    sh 'java -version'
+                    sh 'whoami'
+
+                    echo "GIT_URL: ${GIT_URL}"
+                    echo "GIT_URL_MODIFIED: ${GIT_URL_MODIFIED}"
+                    echo "BUILD_NUMBER ${BUILD_NUMBER}"
+                    echo "BUILD_ID ${BUILD_ID}"
+                    echo "BUILD_DISPLAY_NAME ${BUILD_DISPLAY_NAME}"
+                    echo "JOB_NAME ${JOB_NAME}"
+                    echo "JOB_BASE_NAME ${JOB_BASE_NAME}"
+                    echo "BUILD_TAG ${BUILD_TAG}"
+                    echo "EXECUTOR_NUMBER ${EXECUTOR_NUMBER}"
+                    echo "NODE_NAME ${NODE_NAME}"
+                    echo "NODE_LABELS ${NODE_LABELS}"
+                    echo "WORKSPACE ${WORKSPACE}"
+                    echo "JENKINS_HOME ${JENKINS_HOME}"
+                    echo "JENKINS_URL ${JENKINS_URL}"
+                    echo "BUILD_URL ${BUILD_URL}"
+                    echo "JOB_URL ${JOB_URL}"
+                    echo "CHANGE_AUTHOR_EMAIL ${GIT_COMMIT}"
+                    echo "APIARY_IO_TOKEN_PROP ${APIARY_IO_TOKEN_PROP}"
+                    echo "AZURE_SVC_HOSTNAME_PROP ${AZURE_SVC_HOSTNAME_PROP}"
+                    echo "AZURE_SVC_HOSTNAME_PROP ${AZURE_SVC_HOSTNAME_PROP}"
+                    echo "GIT_SVC_ACOUNT_EMAIL_PROP ${GIT_SVC_ACOUNT_EMAIL_PROP}"
+                    echo "GIT_SVC_ACCOUNT_USER_PROP ${GIT_SVC_ACCOUNT_USER_PROP}"
+                    echo "PROD_WESTEUROPE_AZRGNAME_PROP ${PROD_WESTEUROPE_AZRGNAME_PROP}"
+                    echo "PROD_WESTEUROPE_AZACRNAME_PROP ${PROD_WESTEUROPE_AZACRNAME_PROP}"
                 }
             }
 
