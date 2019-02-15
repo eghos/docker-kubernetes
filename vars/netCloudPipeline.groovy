abd88a32f874311f4835da9990f9345d6609bb20
Onkar Shoker <onkar.shoker@ikea.com>
2019-02-15 14:51:53 +0100

    deploy

--- vars/netCloudPipeline.groovy
+++ vars/netCloudPipeline.groovy
@@ -291,13 +291,13 @@ def call(Map pipelineParams) {
                     }
                 }
                 steps {
-                    script {
+//                    script {
                         sh "~/oc/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/./oc project ${OPENSHIFT_DEV}"
 
                         //sh "~/oc/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/./oc apply -f ./build/istio/configmap-os-onprem-dev.yaml"
                         sh "~/oc/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/./oc new-app --image=${DOCKER_OPENSHIFT_IMAGE}:${DOCKER_VERSION} --name=${DOCKER_OPENSHIFT_IMAGE}"
                         sh "~/oc/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/./oc create route edge ${DOCKER_OPENSHIFT_IMAGE}-route --service=${DOCKER_OPENSHIFT_IMAGE}"
-                    }
+//                    }
                 }
             }
 
