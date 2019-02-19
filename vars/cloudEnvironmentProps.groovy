bb2aad847df66ad47882a0d19da40cfccc05db46
Onkar Shoker <onkar.shoker@ikea.com>
2019-02-19 15:35:09 +0100

    addition of openshift deployment updates

--- vars/cloudEnvironmentProps.groovy
+++ vars/cloudEnvironmentProps.groovy
@@ -16,7 +16,8 @@ class cloudEnvironmentProps{
 
     def AZURE_PROD_SUBSCRIPTION_ID          = "4c58a8b3-26bd-4206-a3ca-6d1fac5d0ed5"
     def AZURE_LOWER_ENV_SUBSCRIPTION_ID     = "6795aaca-7ddd-4af7-ae6d-a984bf8d7744"
-    //tmp
+
+    def OPENSHIFT_SERVICE_ACCOUNT_TOKEN     = "eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJrdWJlcm5ldGVzL3NlcnZpY2VhY2NvdW50Iiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9uYW1lc3BhY2UiOiJvY3AtcGlwZWxpbmVzLWlwaW0taXAiLCJrdWJlcm5ldGVzLmlvL3NlcnZpY2VhY2NvdW50L3NlY3JldC5uYW1lIjoiamVua2lucy10b2tlbi1tMjVkZyIsImt1YmVybmV0ZXMuaW8vc2VydmljZWFjY291bnQvc2VydmljZS1hY2NvdW50Lm5hbWUiOiJqZW5raW5zIiwia3ViZXJuZXRlcy5pby9zZXJ2aWNlYWNjb3VudC9zZXJ2aWNlLWFjY291bnQudWlkIjoiM2EzNjhjOTktZTk4MS0xMWU4LTgyZTQtMDA1MDU2ODUxMmM3Iiwic3ViIjoic3lzdGVtOnNlcnZpY2VhY2NvdW50Om9jcC1waXBlbGluZXMtaXBpbS1pcDpqZW5raW5zIn0.DUM8sW_mhH67NEHSa854qyrdSwmDWPqqCw6yCF5Wg1vkWM3wgpndfHMHbi5ULW2RkghqwrBzO0RCAFAcOW38AwGoqkcOtmlEgBQN5z_9qoXcQw00ze8EkPz0paVDV4Qw1NJ6iI0Z6mYlZNV8OdUKkySPvu4kRDJdqNL20xBnJLkc1Zx2Rh_OfJXtcSutqm2FHBEIzadM_kAezhr_4Awj4YP5aLdosQUqYHi9C4UBdggTrTQpYV-2A3LbNZ0VHYHqG6y6k5XD8hPKOFZFQvlU1jATjk5FG50KCbqyIzUAeoPq7tGI26rTsXYLS_d4sW4-BMwRGYbDzFPIBXrSXoXWng"
 
     def getApiaryIoToken(){
         return APIARY_IO_TOKEN
@@ -56,5 +57,8 @@ class cloudEnvironmentProps{
     }
     def getAwsContainerRepositoryUrl(){
         return AWS_CONTAINER_REPOSITORY_URL
+    }
+    def getOpenshiftServiceAccountToken(){
+        return OPENSHIFT_SERVICE_ACCOUNT_TOKEN()
     }
 }
