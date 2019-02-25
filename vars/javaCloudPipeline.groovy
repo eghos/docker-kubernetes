e0686bdb519faadb1991003c360cd43c2fa26616
Onkar Shoker <onkar.shoker@ikea.com>
2019-02-25 21:43:19 +0100

    deploy

--- vars/javaCloudPipeline.groovy
+++ vars/javaCloudPipeline.groovy
@@ -341,7 +341,7 @@ def call(Map pipelineParams) {
                            export AWS_PROFILE=ikea-tools-system
                            
                            \$(aws ecr get-login --no-include-email --region eu-west-1)
-                           docker tag ${ACRLOGINSERVER}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION} ${AWS_CONTAINER_REPOSITORY_URL_PROP}/${DOCKER_ORG_IMAGE}-${SERVICE_VERSION}:${DOCKER_VERSION}
+                           docker tag ${ACRLOGINSERVER}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION} ${AWS_CONTAINER_REPOSITORY_URL_PROP}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION}
                            docker push ${AWS_CONTAINER_REPOSITORY_URL_PROP}/${DOCKER_ORG_IMAGE}:${DOCKER_VERSION}
                            """
                     }
